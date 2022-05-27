059fb48741da69f49c61bf11655e807f55ccfbcd
shinkay0 <shinkay.register@gmail.com>
2022-05-27 20:13:06 +0200

    adding CLI options

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"time"
 	"training/imgproc/filter"
@@ -8,9 +9,29 @@ import (
 )
 
 func main() {
-	var f filter.Filter = filter.Grayscale{}
-	// t := task.NewWaitGrpTask("./imgs", "output", f)
-	t := task.NewChanTask("./imgs", "output", f, 4)
+	var srcDir = flag.String("src", "", "Input directory")
+	var dstDir = flag.String("dst", "", "Output directory")
+	var filterType = flag.String("filter", "grayscale", "grayscale/blur")
+	var taskType = flag.String("task", "waitgrp", "waitgrp/channel")
+	var poolSize = flag.Int("poolsize", 4, "Wokers pool size for the channel task")
+
+	flag.Parse()
+
+	var f filter.Filter
+	switch *filterType {
+	case "grayscale":
+		f = filter.Grayscale{}
+	case "blur":
+		f = filter.Blur{}
+	}
+
+	var t task.Tasker
+	switch *taskType {
+	case "waitgrp":
+		t = task.NewWaitGrpTask(*srcDir, *dstDir, f)
+	case "channel":
+		t = task.NewChanTask(*srcDir, *dstDir, f, *poolSize)
+	}
 
 	start := time.Now()
 	t.Process()
