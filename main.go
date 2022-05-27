f11c44592db0a6e3a1d4736cd431c34812c7c714
shinkay0 <shinkay.register@gmail.com>
2022-05-27 19:41:09 +0200

    add elapsed time read to main

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"time"
 	"training/imgproc/filter"
 	"training/imgproc/task"
 )
@@ -8,6 +10,10 @@ import (
 func main() {
 	var f filter.Filter = filter.Grayscale{}
 	t := task.NewWaitGrpTask("./imgs", "output", f)
-	t.Process()
 
+	start := time.Now()
+	t.Process()
+	elapsed := time.Since(start)
+
+	fmt.Printf("Image processing took %s\n", elapsed)
 }
